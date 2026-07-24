d6287cf25d8e33a1003cf5036767cdce32c7823e
Nishad9060 <nishadkhatri08@gmail.com>
2026-07-24 21:10:36 +0530

    Fix: Change CardTheme to CardThemeData

--- footballiq_frontend/lib/core/theme/app_theme.dart
+++ footballiq_frontend/lib/core/theme/app_theme.dart
@@ -17,7 +17,7 @@ class AppTheme {
         elevation: 0,
         centerTitle: true,
       ),
-      cardTheme: CardTheme(
+      cardTheme: CardThemeData(
         color: const Color(0xFF1E1E1E),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(16),
